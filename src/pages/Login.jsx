1dee77defb64d4bd648c7343b927627cc2e021a0
pratikpakhale <pratikpakhale20@gmail.com>
2022-06-05 00:39:59 +0530

    ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -145,7 +145,7 @@ function Login() {
             type='password'
             id='password'
             className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-300 focus:border-gray-300 block w-full p-2.5'
-            placeholder='passowrd'
+            placeholder='password'
             required
             value={password}
             onChange={passwordChangeHandler}
