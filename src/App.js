c230191db197e480e739cbb24b3633bca4692420
pratikpakhale <pratikpakhale20@gmail.com>
2022-06-06 23:05:45 +0530

    securing admin routes

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import CredQrCode from './pages/CredQrCode'
 function App() {
   const {
     isAuthenticated,
+    type,
     login,
     newToken,
     setType,
@@ -74,36 +75,46 @@ function App() {
           path='/confirm-registration/:eventId'
           element={<ConfirmRegistration />}
         />
-        <Route path='/admin' element={<Admin />} />'
-        <Route path='/admin/events' element={<AdminEvents />} />
-        <Route path='/admin/events/new' element={<NewEvent />} />
-        <Route path='/admin/events/edit/:eventId' element={<EditEvent />} />
-        <Route path='/admin/students' element={<ManageStudents />} />
-        <Route
-          path='/admin/students/:registrationNumber'
-          element={<ManageStudents />}
-        />
-        <Route path='/admin/creds' element={<ManageCreds />} />
-        <Route path='/admin/creds/qrcode/:credsId' element={<CredQrCode />} />
-        <Route
-          path='/admin/students/managecreds/:registrationNumber'
-          element={<ManageStudentCreds />}
-        />
-        <Route
-          path='/admin/students/managecreds/:registrationNumber/new'
-          element={<AddStudentCreds />}
-        />
-        <Route path='/admin/creds/new' element={<AddCreds />} />
-        <Route
-          path='/admin/students/managecreds/:registrationNumber/edit/:timestamp'
-          element={<EditStudentCredits />}
-        />
-        <Route
-          path='/admin/registrations/:eventId'
-          element={<EventRegistrations />}
-        />
+
         <Route path='/qrcode/:id' element={<CredRewards />} />
         <Route path='/login-to-continue' element={<LoginToContinue />} />
+
+        {isAuthenticated && type === 'admin' && (
+          <>
+            <Route path='/admin' element={<Admin />} />'
+            <Route path='/admin/events' element={<AdminEvents />} />
+            <Route path='/admin/events/new' element={<NewEvent />} />
+            <Route path='/admin/events/edit/:eventId' element={<EditEvent />} />
+            <Route path='/admin/students' element={<ManageStudents />} />
+            <Route
+              path='/admin/students/:registrationNumber'
+              element={<ManageStudents />}
+            />
+            <Route path='/admin/creds' element={<ManageCreds />} />
+            <Route
+              path='/admin/creds/qrcode/:credsId'
+              element={<CredQrCode />}
+            />
+            <Route
+              path='/admin/students/managecreds/:registrationNumber'
+              element={<ManageStudentCreds />}
+            />
+            <Route
+              path='/admin/students/managecreds/:registrationNumber/new'
+              element={<AddStudentCreds />}
+            />
+            <Route path='/admin/creds/new' element={<AddCreds />} />
+            <Route
+              path='/admin/students/managecreds/:registrationNumber/edit/:timestamp'
+              element={<EditStudentCredits />}
+            />
+            <Route
+              path='/admin/registrations/:eventId'
+              element={<EventRegistrations />}
+            />
+          </>
+        )}
+
         <Route path='/*' element={<Navigate to={'/'} />} />
       </Routes>
     </>
